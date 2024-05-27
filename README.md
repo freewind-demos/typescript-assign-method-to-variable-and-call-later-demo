3592025119ddc02d31f1e166a32457e323d34844
Freewind <nowindlee@gmail.com>
2024-05-27 19:50:47 +0800

    typescript-assign-method-to-variable-and-call-later-demo

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-TypeScript Hello World Demo
+TypeScript Assign Method to Variable and Call Later Demo
 ===========================
+
+如果是普通函数定义的method，赋值给变量后其`this`会丢失报错。
+
+如果是`=>`定义的method，则没问题。
+
+但关键是如果我们很难知道要操作的那个方法是怎么定义的
 
 ```
 npm install
--- hello.ts
+++ hello.ts
@@ -1,1 +1,27 @@
-console.log('Hello');
+class User {
+    constructor(private name: string) {
+
+    }
+    hello() {
+        console.log("### hello", this.name)
+    }
+
+    safeHello = () => {
+        console.log("### safeHello", this.name)
+    }
+}
+
+const user = new User('Freewind');
+const hello = user.hello.bind(user);
+hello();
+
+const safeHello = user.safeHello;
+safeHello();
+
+const badHello = user.hello;
+// throw errors
+badHello();
+
+// throw errors
+(user.hello ?? (() => { console.log('fake') }))();
+
